8c5f3beb57c084a44a8e14b423453d7f5956743f
jmlinger <jmlinger10@gmail.com>
2022-05-06 18:11:04 -0300

    feat: adiciona middleware para tratamento de erros.

--- back_end/src/api/app.js
+++ back_end/src/api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const root = require('../controllers/root');
+const { errorHandler } = require('../middlewares');
 
 const app = express();
 app.use(express.json());
 
 app.use(root);
+
+app.use(errorHandler);
 
 module.exports = app;
