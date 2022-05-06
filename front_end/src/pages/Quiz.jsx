79b9857f328342d1961578ef49cd3e11f24d7d79
jmlinger <jmlinger10@gmail.com>
2022-05-06 20:34:52 -0300

    feat: implementa função para enviar formulário e texto que contem placar de respostas.

--- front_end/src/pages/Quiz.jsx
+++ front_end/src/pages/Quiz.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
+import { sendAnswers } from '../services/apiCalls';
 import { AnswersContainer, QuestionList } from '../styles/Quiz';
 
 function Quiz() {
-  const [answers, setAnswers] = useState({})
+  const [answers, setAnswers] = useState({});
+  const [totalScore, setTotalScore] = useState([]);
   
   // console.log(answers);
   const handleClickAndChange = ({ target: {name, value} }) => {
-    
     setAnswers({
       ...answers,
       [name]: value,
     })
   }
 
-    return (
+  const sendForms = async () => {
+    const response = await sendAnswers(answers);
+    const sortedResponse = Object.entries(response).sort((a, b) => b[1] - a[1])
+    setTotalScore(sortedResponse);
+  }
+
+  const answeredQuestions = totalScore.reduce((acc, cur) => acc[1] + cur[1], 0)
+
+  return (
+    <>
       <form>
         <fieldset>
           <QuestionList>
@@ -100,12 +110,37 @@ function Quiz() {
               <p>{`${answers.pergunta4 ? answers.pergunta4.length : 0}/200`}</p>
             </li>
           </QuestionList>
-          <button type='submit' disabled={Object.keys(answers).length !== 4}>
+          <button
+            type='submit'
+            disabled={Object.keys(answers).length !== 4}
+            onClick={() => sendForms()}
+          >
             Enviar
           </button>
         </fieldset>
       </form>
-    );
+      <fieldset>
+        <label htmlFor="total-answers">Total</label>
+        <h3 id="total-answers">
+          {answeredQuestions}
+        </h3>
+        {
+          totalScore.map((score) => {
+            <div>
+              <label htmlFor="">{score[0]}l</label>
+              <h3>{score[1]}</h3>
+              <label htmlFor="">
+                %{score[0].substring(9)}
+              </label>
+              <h3>
+                {score[1]/answeredQuestions * 100}%
+              </h3>
+            </div>
+          })
+        }
+      </fieldset>
+    </>
+  );
 }
 
 export default Quiz;
