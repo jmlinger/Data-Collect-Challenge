d6622cede6ac39404c8e32cdd8753da8e6ee398d
jmlinger <jmlinger10@gmail.com>
2022-05-07 20:33:57 -0300

    feat: adiciona o cors.

--- back_end/src/api/app.js
+++ back_end/src/api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const root = require('../controllers/root');
 const { errorHandler } = require('../middlewares');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use(root);
 
